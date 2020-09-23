e966966e9e01fbf1126e08af2ca76e96b647ecb3
Gourav-raj <gouravrj9654@gmail.com>
2020-09-23 18:00:48 +0530

    Updates

---
+++ precache-manifest.c8c053d2cfaae2ca1f25fe4b637b6a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4018f8111d8f027ad8a3c8d3a546a7f",
+    "url": "/Amazon-Clone_gr/index.html"
+  },
+  {
+    "revision": "99e3f74d39082fe2a98c",
+    "url": "/Amazon-Clone_gr/static/css/main.c9fbf541.chunk.css"
+  },
+  {
+    "revision": "4e60de4b42d5a356dc76",
+    "url": "/Amazon-Clone_gr/static/js/2.a0e64fc6.chunk.js"
+  },
+  {
+    "revision": "1cd206e4f52c8f356667f4e1ed8ba9f3",
+    "url": "/Amazon-Clone_gr/static/js/2.a0e64fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e3f74d39082fe2a98c",
+    "url": "/Amazon-Clone_gr/static/js/main.38eca2d4.chunk.js"
+  },
+  {
+    "revision": "2fcec2ddc85f7156646f",
+    "url": "/Amazon-Clone_gr/static/js/runtime-main.9c730c78.js"
+  }
+]);
